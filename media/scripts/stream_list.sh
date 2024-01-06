b51f349526ea326d3afa0c80f378034e9992433d
Kent0810 <kent.bui207ktmt@hcmut.edu.vn>
2024-01-06 23:23:10 +0700

    note for command

--- media/scripts/stream_list.sh
+++ media/scripts/stream_list.sh
@@ -3,5 +3,14 @@
 curl -XGET -H 'Authorization: Basic dGhlc2lzOnExamsyM2kxOQ==' \
     'http://127.0.0.1:7956/v1/vhosts/default/apps/camera/streams'
 
+# TO GET ALL STREAM
+
+## FOR LTD
+
 curl -XGET -H 'Authorization: Basic dGhlc2lzOnExamsyM2kxOQ==' \
-    'http://127.0.0.1:7956/v1/vhosts/default/apps/camera/streams/ltdtest'
+    'http://127.0.0.1:7956/v1/vhosts/default/apps/ltd/streams'
+
+## FOR CLOUD 
+
+curl -XGET -H 'Authorization: Basic dGhlc2lzOnExamsyM2kxOQ==' \
+    'http://127.0.0.1:7956/v1/vhosts/default/apps/cloud/streams'
