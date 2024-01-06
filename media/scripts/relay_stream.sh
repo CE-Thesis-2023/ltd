834347ec558490c2f194511a62fe822ddf043a03
Nguyen Tran <nguyentt1@vnpay.vn>
2024-01-06 13:30:00 +0700

    added relay script

--- media/scripts/relay_stream.sh
+++ media/scripts/relay_stream.sh
@@ -7,6 +7,6 @@ curl -XPOST -H 'Authorization: Basic dGhlc2lzOnExamsyM2kxOQ==' -H "Content-type:
     "variantNames": []
   },
   "protocol": "srt",
-  "url": "srt://103.165.142.44:7958/camera/ltdtest?mode=caller&latency=120000&timeout=500000",
+  "url": "srt://103.165.142.44:7958?streamid=srt%3A%2F%2F103.165.142.44%3A7958%2Fcamera%2Fltdtest&mode=caller",
   "streamKey": ""
 }' 'http://127.0.0.1:7956/v1/vhosts/default/apps/ltd:startPush'
