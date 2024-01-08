0a9c75ba4d6f4183916422bd27fe54e7894846af
Nguyen Tran <nguyentt1@vnpay.vn>
2024-01-08 13:22:39 +0700

    restructure the files

--- media/camera/smart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -XGET \
-    'http://admin:bkcamera2023@192.168.8.55/ISAPI/Streaming/status'
--- media/scripts/pull_down.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-jsonRequest='{"cameraId":"32845204","pan":0,"tilt":-60,"stopAfterSeconds":2}'
-cameraId='32845204'
-
-mqtt pub -t ptzctrl/$cameraId \
-    -m "$jsonRequest" \
-    -h 103.165.142.44 \
-    -p 9093
