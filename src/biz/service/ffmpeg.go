c5f4e2d821fe8036822e77689e4c521f78ca3e3a
Nguyen Tran Trung <nguyentt1@vnpay.vn>
2024-04-05 17:31:02 +0700

    updated

--- src/biz/service/ffmpeg.go
+++ src/biz/service/ffmpeg.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os/exec"
 	"path/filepath"
+	"sync"
 
 	"github.com/CE-Thesis-2023/ltd/src/helper"
 	"github.com/CE-Thesis-2023/ltd/src/internal/configs"
@@ -15,8 +16,57 @@ import (
 	"github.com/CE-Thesis-2023/ltd/src/models/db"
 	"github.com/CE-Thesis-2023/ltd/src/models/events"
 	"github.com/CE-Thesis-2023/ltd/src/models/ms"
+	"github.com/CE-Thesis-2023/ltd/src/models/rest"
 	"go.uber.org/zap"
 )
+
+type onGoingProcess struct {
+	SourceUrl      string
+	DestinationUrl string
+	proc           *exec.Cmd
+}
+
+func (c *onGoingProcess) Cancel(ctx context.Context) error {
+	if c.proc != nil {
+		return c.proc.Cancel()
+	}
+	return nil
+}
+
+type mediaService struct {
+	onGoingProcesses map[string]*onGoingProcess
+	mu               sync.Mutex
+}
+
+func NewMediaService() MediaServiceInterface {
+	return &mediaService{
+		onGoingProcesses: map[string]*onGoingProcess{},
+	}
+}
+
+func (s *mediaService) Shutdown() {
+	logger.SInfo("shutting down media service")
+	for cameraId, p := range s.onGoingProcesses {
+		if p.proc != nil {
+			if p.proc.Cancel != nil {
+				if err := p.proc.Cancel(); err != nil {
+					logger.SDebug("error shutting down FFmpeg process", zap.Error(err))
+					continue
+				}
+				delete(s.onGoingProcesses, cameraId)
+				logger.SDebug("shutdown FFmpeg process", zap.String("cameraId", cameraId))
+			}
+		}
+	}
+	logger.SDebug("released streaming pool")
+}
+
+type MediaServiceInterface interface {
+	RequestFFmpegRtspToSrt(ctx context.Context, camera *db.Camera, req *events.CommandStartStreamInfo) error
+	CancelFFmpegRtspToSrt(ctx context.Context, camera *db.Camera) error
+	ListOngoingStreams(ctx context.Context) (*rest.DebugListStreamsResponse, error)
+	Shutdown()
+}
 
 func (s *mediaService) RequestFFmpegRtspToSrt(ctx context.Context, camera *db.Camera, req *events.CommandStartStreamInfo) error {
 	logger.SInfo("requested starting to perform RTSP to SRT transcoding stream", zap.String("request", req.CameraId))
@@ -195,4 +245,19 @@ func (s *mediaService) buildRtspStreamUrl(camera *db.Camera) string {
 	url := u.String()
 	logger.SDebug("buildRtspStreamUrl: stream url", zap.String("url", url))
 	return url
+}
+
+func (s *mediaService) ListOngoingStreams(ctx context.Context) (*rest.DebugListStreamsResponse, error) {
+	logger.SDebug("ListOngoingStreams: request received")
+	streams := s.onGoingProcesses
+	resp := &rest.DebugListStreamsResponse{}
+	for cameraId, s := range streams {
+		resp.Streams = append(resp.Streams, rest.StreamInfo{
+			CameraId:       cameraId,
+			SourceUrl:      s.SourceUrl,
+			DestinationUrl: s.DestinationUrl,
+		})
+	}
+	logger.SDebug("ListOngoingStreams: streams", zap.Reflect("streams", resp))
+	return resp, nil
 }
